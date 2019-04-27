41c7682cc04444d9fad2a6be917a9c763cb32c59
Anushree Saha <as2496@njit.edu>
2019-04-27 17:38:14 -0400

    adding demo example of design pattern SOLID

---
+++ DesignPattern/JournalSingleResponsibility.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DesignPattern
+{
+    public class JournalSingleResponsibility
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{ ++count}: { text}");
+            return count; // memento pattern!
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        // breaks single responsibility principle
+        public void Save(string filename, bool overwrite = false)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        public void Load(string filename)
+        {
+
+        }
+
+        public void Load(Uri uri)
+        {
+
+        }
+        // handles the responsibility of persisting objects
+        public class Persistence
+        {
+            public void SaveToFile(JournalSingleResponsibility journal, string filename, bool overwrite = false)
+            {
+                if (overwrite || !File.Exists(filename))
+                    File.WriteAllText(filename, journal.ToString());
+            }
+        }
+    }
+}
---
+++ DesignPattern/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using static DesignPattern.JournalSingleResponsibility;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            var j = new JournalSingleResponsibility();
+            j.AddEntry("I cried today.");
+            j.AddEntry("I ate a bug.");
+            Console.WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"E:\GitRepo\DisignPattern\journal.txt";
+            p.SaveToFile(j, filename);
+            Process.Start(filename);
+            */
+
+
+
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            var pf = new ProductFilter();
+            Console.WriteLine("Green products (old):");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+                Console.WriteLine($" - {p.Name} is green");
+
+            // ^^ BEFORE
+
+            // vv AFTER
+            var bf = new BetterFilter();
+            Console.WriteLine("Green products (new):");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+                Console.WriteLine($" - {p.Name} is green");
+
+            Console.WriteLine("Large products");
+            foreach (var p in bf.Filter(products, new SizeSpecification(Size.Large)))
+                Console.WriteLine($" - {p.Name} is large");
+
+            Console.WriteLine("Large blue items");
+            foreach (var p in bf.Filter(products,
+                new AndSpecification<Product>(new ColorSpecification(Color.Blue), new SizeSpecification(Size.Large)))
+            )
+            {
+                Console.WriteLine($" - {p.Name} is big and blue");
+            }
+        }
+    }
+}
